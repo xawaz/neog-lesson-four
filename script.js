41f414c67abe632e3a293b9b2c89729fa9774e50
zf <www.fawazahmed1@gmail.com>
2021-08-07 19:01:35 +0530

    add: log inputArea on click

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ var i=0;
 btn.addEventListener("click",function call(){
     return console.log("clicked!");
 });
+
+var txtInput=document.querySelector(".txt-input");
+txtInput.addEventListener("click", function callback(){
+    console.log(txtInput);
+})
