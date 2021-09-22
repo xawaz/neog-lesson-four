a5bc4328a085e4e38c68a2f1dfbc2c720f2dbd42
zf <www.fawazahmed1@gmail.com>
2021-09-22 19:05:43 +0530

    add page

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var btn=document.querySelector(".btn-translate");
+var txtInput=document.querySelector(".txt-input");
+var txtOutput=document.querySelector(".txt-output");
+
 var i=0;
 btn.addEventListener("click",function call(){
-    return console.log("clicked!");
+    console.log("clicked!");
+    var text=txtInput.value;
+    var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"+"?text="+text;
+    fetch(url)
+    .then(response=>response.json())
+    .then(function process(json){
+        var translated=response.contents.translated;
+        txtOutput.innerText=translated;
+    })
 });
 
-var txtInput=document.querySelector(".txt-input");
+
 txtInput.addEventListener("click", function callback(){
-    console.log(txtInput);
+    console.log(txtInput); 
 })
